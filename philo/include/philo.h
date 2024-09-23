d07e969a13b4673b40b21083778b1fa933625296
kfukuhar <kfukuhar@student.42tokyo.jp>
2024-09-23 16:30:35 +0900

    init utils

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,13 +6,14 @@
 /*   By: kfukuhar <kfukuhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 14:52:20 by kfukuhar          #+#    #+#             */
-/*   Updated: 2024/09/23 15:43:25 by kfukuhar         ###   ########.fr       */
+/*   Updated: 2024/09/23 16:29:58 by kfukuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <limits.h>
 # include <pthread.h>
 # include <stdbool.h>
 # include <stdio.h>
@@ -20,54 +21,56 @@
 # include <sys/time.h>
 # include <unistd.h>
 
-typedef struct s_fork t_fork;
-typedef struct s_table t_table;
-typedef struct s_philo t_philo;
+typedef struct s_fork	t_fork;
+typedef struct s_table	t_table;
+typedef struct s_philo	t_philo;
 
 typedef struct s_fork
 {
-	pthread_mutex_t	fork;
-	int				fork_id;
-}					t_fork;
+	pthread_mutex_t		fork;
+	int					fork_id;
+}						t_fork;
 
 typedef struct s_table
 {
-	long int		philo_nbr;
-	long int		time_to_die;
-	long int		time_to_eat;
-	long int		time_to_sleep;
-	long int		nbr_limit_meals;
-	long int		start_simulation;
-	t_fork			*forks;
-	t_philo			*philos;
-}					t_table;
+	size_t				philo_nbr;
+	size_t				time_to_die;
+	size_t				time_to_eat;
+	size_t				time_to_sleep;
+	size_t				nbr_limit_meals;
+	size_t				start_simulation;
+	t_fork				*forks;
+	t_philo				*philos;
+}						t_table;
 
 typedef struct s_philo
 {
-	int				id;
-	long int		meals_counter;
-	bool			full;
-	long int		last_meal_time;
-	t_fork			*l_fork;
-	t_fork			*r_fork;
-	pthread_t		thread_id;
-	t_table			*table;
-}					t_philo;
+	int					id;
+	long int			meals_counter;
+	bool				full;
+	long int			last_meal_time;
+	t_fork				*l_fork;
+	t_fork				*r_fork;
+	pthread_t			thread_id;
+	t_table				*table;
+}						t_philo;
 
 // main funcs
-bool    validate_input(int argc, char **argv);
+bool					validate_input(int argc, char **argv);
 
-//utils
-bool    print_err(const char *msg);
+// utils
+bool					print_err(const char *msg);
+int						ft_isdigit(int i);
+int						ft_atoi(const char *str);
 
 // dev
 # include <string.h>
 
 typedef struct s_test
 {
-	int				sum;
-	char			*str;
-	pthread_mutex_t	mutex;
-}					t_test;
+	int					sum;
+	char				*str;
+	pthread_mutex_t		mutex;
+}						t_test;
 
 #endif
---
+++ philo/src/ft_atol.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfukuhar <kfukuhar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 16:28:35 by kfukuhar          #+#    #+#             */
+/*   Updated: 2024/09/23 16:28:37 by kfukuhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static int	ft_isspace(char c)
+{
+	return (
+		c == '\t'
+		|| c == '\n'
+		|| c == '\v'
+		|| c == '\f'
+		|| c == '\r'
+		|| c == ' '
+	);
+}
+
+int	ft_atoi(const char *str)
+{
+	size_t	index;
+	size_t	is_mi;
+	long	result;
+
+	index = 0;
+	while (ft_isspace(str[index]))
+		index++;
+	if (str[index] == '-')
+		is_mi = 1;
+	if (str[index] == '-' || str[index] == '+')
+		index++;
+	result = 0;
+	while (ft_isdigit(str[index]) && str[index])
+	{
+		if (is_mi != 1 && result > (LONG_MAX - (str[index] - '0')) / 10)
+			return ((int)LONG_MAX);
+		else if (is_mi == 1 && - result < (LONG_MIN + (str[index] - '0')) / 10)
+			return ((int)LONG_MIN);
+		result = 10 * result + (str[index] - '0');
+		index++;
+	}
+	if (is_mi == 1)
+		return ((int)-1 * result);
+	else
+		return ((int)result);
+}
