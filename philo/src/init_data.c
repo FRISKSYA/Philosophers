c0ed3ec305a45123a2bf2cb26fd4c1d1e17b8f58
kfukuhar <kfukuhar@student.42tokyo.jp>
2024-09-23 18:22:57 +0900

    fixed norm

--- philo/src/init_data.c
+++ philo/src/init_data.c
@@ -6,27 +6,27 @@
 /*   By: kfukuhar <kfukuhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 17:54:33 by kfukuhar          #+#    #+#             */
-/*   Updated: 2024/09/23 18:17:41 by kfukuhar         ###   ########.fr       */
+/*   Updated: 2024/09/23 18:22:46 by kfukuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 // TODO: fill values
-static int  init_fork(t_table *table)
+static int	init_fork(t_table *table)
 {
-    table->forks = (t_fork *)malloc(table->philo_nbr * sizeof(t_fork));
-    if (table->forks == NULL)
-        return (EXIT_FAILURE);
-    return (EXIT_SUCCESS);
+	table->forks = (t_fork *)malloc(table->philo_nbr * sizeof(t_fork));
+	if (table->forks == NULL)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
 
-static int  init_philo(t_table *table)
+static int	init_philo(t_table *table)
 {
-    table->philos = (t_philo *)malloc(table->philo_nbr * sizeof(t_philo));
-    if (table->philos == NULL)
-        return (EXIT_FAILURE);
-    return (EXIT_SUCCESS);
+	table->philos = (t_philo *)malloc(table->philo_nbr * sizeof(t_philo));
+	if (table->philos == NULL)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
 
 int	init_data(t_table **table, char **argv)
@@ -37,18 +37,17 @@ int	init_data(t_table **table, char **argv)
 	if (*table == NULL)
 		return (EXIT_FAILURE);
 	i = 1;
-    (*table)->philo_nbr = ft_atoi(argv[i++]);
-    (*table)->time_to_die = ft_atoi(argv[i++]);
-    (*table)->time_to_eat = ft_atoi(argv[i++]);
-    (*table)->time_to_sleep = ft_atoi(argv[i++]);
-    if (argv[i])
-        (*table)->nbr_limit_meals = ft_atoi(argv[i]);
-    else
-        (*table)->nbr_limit_meals = 0;
-    if (init_fork(*table) == EXIT_FAILURE)
-        return (EXIT_FAILURE);
-    if (init_philo(*table) == EXIT_FAILURE)
-        return (EXIT_FAILURE);
-    else
-	    return (EXIT_SUCCESS);
+	(*table)->philo_nbr = ft_atoi(argv[i++]);
+	(*table)->time_to_die = ft_atoi(argv[i++]);
+	(*table)->time_to_eat = ft_atoi(argv[i++]);
+	(*table)->time_to_sleep = ft_atoi(argv[i++]);
+	if (argv[i])
+		(*table)->nbr_limit_meals = ft_atoi(argv[i]);
+	else
+		(*table)->nbr_limit_meals = 0;
+	if (init_fork(*table) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	if (init_philo(*table) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
